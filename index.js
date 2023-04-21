d7abf75d0844119a26c9f18a7e885ea7ea83313e
Yuriy Liubinskyi <leorentex@gmail.com>
2023-04-21 15:56:47 +0300

    commit app

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const accountRoutes = require('./routes/account')
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 app.use(session({
     secret: config.secret,
     resave: false,
